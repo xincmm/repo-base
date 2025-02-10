5c52ffad5200f592ff8dd0d19b77de4f3e47a17e
Akuya Ekorot <akuyaekorot@gmail.com>
2025-02-10 10:34:28 +0300

    wip

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -13,17 +13,8 @@ export async function POST(req: NextRequest) {
 
   const { messages } = (await req.json()) as { messages: CoreMessage[] };
 
-  const repository = await db.query.repos.findFirst({
-    where: (f, o) => o.eq(f.id, Number(repoId)),
-    columns: {
-      id: true,
-      name: true,
-      description: true,
-    },
-  });
-
-  // const chatAgent = mastra.getAgent("chatAgent");
   const chatAgent = mastra.getAgent("repoExplorer");
+  const context: CoreMessage[] = [];
 
   let threadId: string;
 
@@ -41,34 +32,34 @@ export async function POST(req: NextRequest) {
 
     cookieStore.set(`threadId:${repoId}`, newThreadId);
     threadId = newThreadId;
-  } else {
-    threadId = cookieThreadId.value;
-  }
 
-  const result = await chatAgent.stream(
-    [
-      {
-        role: "user",
-        content: "My name is Akuya, say Hi Akuya first before replying",
+    const repository = await db.query.repos.findFirst({
+      where: (f, o) => o.eq(f.id, Number(repoId)),
+      columns: {
+        id: true,
+        name: true,
+        description: true,
       },
-      ...messages,
-    ],
-    {
-      maxSteps: 20,
-      context: [
-        {
-          role: "system",
-          content: `
+    });
+
+    context.push({
+      role: "system",
+      content: `
 Use this as the repository details: ${JSON.stringify(repository)}.
 Don't mention the repository ID back to the user since it's an internal implementation detail.
 Don't mention the name of the tools to the user.
 `,
-        },
-      ],
-      threadId,
-      resourceId: String(repoId),
-    },
-  );
+    });
+  } else {
+    threadId = cookieThreadId.value;
+  }
+
+  const result = await chatAgent.stream(messages, {
+    maxSteps: 20,
+    context,
+    threadId,
+    resourceId: String(repoId),
+  });
 
   return result.toDataStreamResponse();
 }
